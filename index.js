2468d8517e011d402035324bb61f3de7ab965cb0
ayush-mavani <ayushmavani6@gmail.com>
2025-08-07 22:48:23 +0530

    Update default server port to 8080

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
